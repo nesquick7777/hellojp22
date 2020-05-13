6ec6aa5679753bb9aaca69883d213f3d6537b396
nesquick7777 <nesquick7777>
2020-05-13 20:36:39 +0200

    galerijaa

--- galerija.sql
+++ galerija.sql
@@ -2,3 +2,24 @@
 drop database if exists galerija;
 create database galerija;
 use galerija;
+
+create table galerija(
+    sifra       int not null primary key auto_increment,
+    naziv       varchar(50) not null,
+    opis        text,
+    cijena      decimal(18,2)
+);
+
+create table osoba(
+    sifra int not null primary key auto_increment,
+    ime varchar(50) not null,
+    prezime varchar(50) not null,
+    galerija int
+);
+
+alter table osoba add foreign key (galerija) references galerija(sifra);
+
+select * from osoba;
+
+insert into galerija (naziv, opis, cijena)
+values
