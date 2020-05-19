b34d5ae5b24db15887cdcec24ff68889ccf44e6d
nesquick7777 <nesquick7777@gmail.com>
2020-05-19 21:42:14 +0200

    Izbaceni korisnik i osoba

--- music.sql
+++ music.sql
@@ -3,39 +3,6 @@ drop database if exists music;
 create database music;
 use music;
 
-create table dopuštenje(
-    sifra int not null primary key auto_increment,
-    uloga int not null,
-    mozeocjenjivati varchar(50) not null,
-    mozedodavatidatu varchar(50) not null,
-    mozebanovatikorisnike varchar(50) not null,
-    mozedodavatimoderatore varchar(50) not null,
-    mozebrisatimoderatore varchar(50) not null
-);
-
-create table uloga(
-    sifra int not null primary key auto_increment,
-    ime varchar(50) not null,
-    opis varchar(50) not null,
-    dopuštenje int
-);
-
-create table korisnik(
-    sifra int not null primary key auto_increment,
-    username varchar(50) not null,
-    lozinka varchar(50) not null,
-    uloga int not null,
-    osoba int not null
-    
-);
-
-create table osoba(
-    sifra int not null primary key auto_increment,
-    ime varchar(50) not null,
-    prezime varchar(50) not null,
-    email varchar(100) not null
-);
-
 # Kasnije implemetirati članove
 create table umjetnik(
 sifra int not null primary key auto_increment,
@@ -49,7 +16,7 @@ datumkraja int
 
 create table žanr(
 sifra int not null primary key auto_increment,
-ime varchar(50)
+naziv varchar(50)
 );
 
 create table diskografija(
@@ -85,13 +52,6 @@ diskografija int
 #create table top100
 
 
-alter table korisnik add foreign key (uloga) references uloga(sifra);
-alter table korisnik add foreign key (osoba) references osoba(sifra);
-
-alter table dopuštenje add foreign key (uloga) references uloga(sifra);
-
-alter table uloga add foreign key (dopuštenje) references dopuštenje(sifra);
-
 alter table umjetnik add foreign key (žanr) references žanr(sifra);
 alter table umjetnik add foreign key (diskografija) references diskografija(sifra);
 
@@ -105,11 +65,6 @@ alter table album add foreign key (diskografija ) references diskografija(sifra)
 alter table singl add foreign key (žanr) references žanr(sifra);
 alter table singl add foreign key (diskografija ) references diskografija(sifra);
 
-insert into osoba (prezime, ime, email)
-values
-('Bojan','Drezgic','nesquick7777@gmail.com'),
-('Bole','Drezga','nesquick77777@gmail.com'),
-('Bojan','Borić','nesquick777777@gmail.com');
 
 insert into uloga (ime, opis)
 values
@@ -117,18 +72,6 @@ values
 ('Moderator','Odobrava promjene, banuje ljude'),
 ('Korisnik','Ocjenjuje, predlaže promjene');
 
-insert into dopuštenje (uloga,mozeocjenjivati,mozedodavatidatu,mozebanovatikorisnike,mozedodavatimoderatore,mozebrisatimoderatore)
-values
-(1,'TRUE','TRUE','TRUE','TRUE','TRUE'),
-(2,'TRUE','TRUE','TRUE','FALSE','FALSE'),
-(3,'TRUE','TRUE','FALSE','FALSE','FALSE');
-update uloga set dopuštenje = 1 where sifra =1;
-
-insert into korisnik (osoba,uloga, username, lozinka)
-values
-(1,1,'Nesquick7777','q1w2e3r4'),
-(2,3,'Nesquick77777','1234'),
-(3,2,'Nesquick777777','4321');
 
 insert into žanr(ime) 
 values
